0eb2ae37f5a7903f87704e5bdcce5c139d28fcf2
M Jeffrey Castillo <mjeffcastillo@gmail.com>
2021-08-31 16:07:58 -0400

    .

---
+++ 01-java-fundamentals/javaFun/01-first-java-program/FirstJavaProgram.java
@@ -0,0 +1,7 @@
+public class FirstJavaProgram {
+    public static void main(String[] args) {
+        System.out.println("My name is Mark Jeffrey C");
+        System.out.println("I am 41 years old");
+        System.out.println("My hometown is Matawan, NJ");
+    }
+}
