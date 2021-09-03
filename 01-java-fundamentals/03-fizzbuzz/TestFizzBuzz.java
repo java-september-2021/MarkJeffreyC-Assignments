cf8e49c3285195bb57d6cca8cc4cc045b0b71e56
M Jeffrey Castillo <mjeffcastillo@gmail.com>
2021-09-03 12:05:09 -0400

    FizzBuzz Assignment

---
+++ 01-java-fundamentals/03-fizzbuzz/TestFizzBuzz.java
@@ -0,0 +1,30 @@
+public class TestFizzBuzz {
+    // This will be the launch point for the application
+    // For now, we are using this method to test our 
+    // FizzBuzz methods
+    public static void main(String[] args) {
+        FizzBuzz tester = new FizzBuzz();
+
+        System.out.println(tester.fizzBuzz(9));     // Fizz
+        System.out.println(tester.fizzBuzz(10));    // Buzz
+        System.out.println(tester.fizzBuzz(15));    // FizzBuzz
+        System.out.println(tester.fizzBuzz(16));    // 16
+        System.out.println(tester.fizzBuzz(2));     // 2
+
+        // Uncomment the following for a more comprehensive test:
+        for (int i = 1; i <= 50; i++) {
+            System.out.println(tester.fizzBuzz(i));
+        }
+
+        System.out.println(tester.fizzBuzz(9,"three", "five", "three five"));
+        System.out.println(tester.fizzBuzz(10,"three", "five", "three five"));
+        System.out.println(tester.fizzBuzz(15,"three", "five", "three five"));
+        System.out.println(tester.fizzBuzz(16,"three", "five", "three five"));
+        System.out.println(tester.fizzBuzz(2,"three", "five", "three five"));
+
+        // for (int i = 1; i <= 50; i++) {
+        //     System.out.println(tester.fizzBuzz(i, "three", "five", "three five"));
+        // }
+    }
+}
+
