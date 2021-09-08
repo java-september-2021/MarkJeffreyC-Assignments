930881d009f822abc02bb1684d878f1e34ffa4ec
M Jeffrey Castillo <mjeffcastillo@gmail.com>
2021-09-08 12:01:31 -0400

    Map of the Hashmatique Assignment

--- 01-java-fundamentals/07-map-of-the-hashmatique/MapOfHash.java
+++ 01-java-fundamentals/07-map-of-the-hashmatique/MapOfHash.java
@@ -2,9 +2,6 @@ import java.util.HashMap;
 
 public class MapOfHash{
     public HashMap<String, String> trackList = new HashMap<String, String>();
-    // trackList.put("Billy Jean", "Billy Jean is not my lover, She's just a girl who claims that I am the one");
-    // trackList.put("Pretty Little Thing", "It's alright, it's alright, Tell me you need me");
-    // trackList.put("Bad", "Because I'm Bad, I'm Bad, You know I'm Bad, I'm Bad");
 
     public String getLyric(String title){
         return trackList.get(title);
